8606a1395b09044c0599798c46043cf369ffd31a
Matthew Todd <mathtodd05@gmail.com>
2026-05-11 18:40:56 -0400

    Create ecosystem.config.js

---
+++ src/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: "OspreyDB-API",
+        script: "main-api.py",
+        interpreter: "../.venv/bin/python",
+        env: {
+
+        },
+      },
+    ],
+  };
